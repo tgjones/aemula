8be1594e762520be40ed49a8c912a03617980517
Tim Jones <tim@timjones.io>
2025-07-24 18:20:11 +0100

    Fix vsync

--- src/Aemula.Tests/Emulation/Output/TelevisionTests.cs
+++ src/Aemula.Tests/Emulation/Output/TelevisionTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -14,14 +15,14 @@ internal class TelevisionTests
 
         // NTSC signal format:
         //
-        // Scanline:        63.5 µs (15.734 kHz)
+        // Scanline:          63.5 µs (15.734 kHz)
         //   Blanking:        10.9 µs
         //     Front Porch:    1.5 µs at   0 IRE
         //     Sync Tip:       4.7 µs at -40 IRE, 0V
         //     Back Porch:     4.7 µs at   0 IRE
         //       Breezeway:    0.6 µs at   0 IRE
         //       Color Burst:  2.5 µs centred at 0 IRE, with 40 IRE peak-to-peak amplitude, 9 +- 1 cycles
-        //     Active Video:   52.6 µs between 7.5 +- 2.5 and 100 IRE
+        //   Active Video:    52.6 µs between 7.5 +- 2.5 and 100 IRE
         //
         // IRE:
         //   -40 IRE = -286mV
@@ -37,12 +38,18 @@ internal class TelevisionTests
         const float ntscSamplesPerMicrosecond = ntscSamplesPerSecond / 1_000_000f;
 
         const int syncLevel = 4; // Sync level should be 0, but we allow some leeway.
+        const int blankLevel = 40 / 140 * 200;
 
         var syncSamples = 0;
         var foundHSync = false;
-        var foundHSyncAtLeastOnce = false;
+        var foundVSync = false;
 
         var samples = new List<double>();
+
+        var yPos = 0;
+        var xPos = 0;
+
+        var bitmap = new System.Drawing.Bitmap(2000, 2000);
 
         for (var i = 0; i < ntscBytes.Length; i++)
         {
@@ -52,36 +59,66 @@ internal class TelevisionTests
 
             samples.Add(b / 200.0f);
 
+            var isBelowSyncLevel = false;
+
             if (b <= syncLevel)
             {
                 syncSamples++;
+                isBelowSyncLevel = true;
             }
+
+            var isBlanked = b < blankLevel;
 
             // HSYNC is 4.7 microseconds long, +- 0.2 microseconds.
             const float hSyncNominalDurationInMicroseconds = 4.7f;
             const float hSyncToleranceInMicroseconds = 0.2f;
             const float hSyncMinimumDurationInMicroseconds = hSyncNominalDurationInMicroseconds - hSyncToleranceInMicroseconds;
             const int hSyncDuration = (int)(hSyncMinimumDurationInMicroseconds * ntscSamplesPerMicrosecond);
-            if (syncSamples >= hSyncDuration)
+            if (!isBelowSyncLevel && syncSamples >= hSyncDuration)
             {
-                if (foundHSyncAtLeastOnce)
-                {
-                    break;
-                }
+                //if (foundHSyncAtLeastOnce)
+                //{
+                //    break;
+                //}
                 foundHSync = true;
+                xPos = 0;
+                yPos++;
             }
 
-            if (foundHSync && b > syncLevel)
+            // VSYNC
+            const int vSyncDuration = 380; // TODO
+            if (!isBelowSyncLevel && syncSamples >= vSyncDuration)
             {
-                foundHSyncAtLeastOnce = true;
-                foundHSync = false;
+                foundVSync = true;
+                yPos = 0;
+            }
+
+            if (!isBelowSyncLevel)
+            {
                 syncSamples = 0;
             }
+
+            if (!isBlanked)
+            {
+                // Active video.
+                var y = (b - blankLevel) * 2; // Normalize to 0 IRE.
+                y = Math.Clamp(y, 0, 255);
+
+                var actualX = Math.Clamp(xPos, 0, bitmap.Width - 1);
+                bitmap.SetPixel(actualX, yPos, System.Drawing.Color.FromArgb(255, y, y, y));
+            }
+
+            // TODO: If it's been a certain amount of time since the last horizontal sync, we should assume the horizontal sync is starting.
+            // TODO: If it's been a certain amount of time since the last vertical sync, we should assume the vertical sync is starting.
+
+            xPos++;
         }
 
-        var myPlot = new ScottPlot.Plot(4000, 600);
-        myPlot.AddSignal(samples.ToArray());
-        myPlot.SaveFig("signal.png");
+        bitmap.Save("ntsc.png");
+
+        //var myPlot = new ScottPlot.Plot(4000, 600);
+        //myPlot.AddSignal(samples.ToArray());
+        //myPlot.SaveFig("signal.png");
     }
 
     [Test]
