45f45aaa57e4afc5a0559772cb847b924a950033
Tim Jones <tim@timjones.io>
2026-08-13 19:13:16 +0800

    Switch 74LS00 pins to auto-properties
    Replaces the manual backing-field + set-only-property pattern with
    auto-properties using a private getter, which reads cleaner while
    keeping inputs write-only from outside the chip.

--- src/Aemula/Emulation/Chips/Ttl7400Chip.cs
+++ src/Aemula/Emulation/Chips/Ttl7400Chip.cs
@@ -2,35 +2,19 @@ namespace Aemula.Emulation.Chips;
 
 public sealed class Ttl7400Chip
 {
-    private bool _a1;
-    public bool A1 { set => _a1 = value; }
+    public bool A1 { private get; set; }
+    public bool B1 { private get; set; }
+    public bool Y1 => !(A1 && B1);
 
-    private bool _b1;
-    public bool B1 { set => _b1 = value; }
+    public bool A2 { private get; set; }
+    public bool B2 { private get; set; }
+    public bool Y2 => !(A2 && B2);
 
-    public bool Y1 => !(_a1 && _b1);
+    public bool A3 { private get; set; }
+    public bool B3 { private get; set; }
+    public bool Y3 => !(A3 && B3);
 
-    private bool _a2;
-    public bool A2 { set => _a2 = value; }
-
-    private bool _b2;
-    public bool B2 { set => _b2 = value; }
-
-    public bool Y2 => !(_a2 && _b2);
-
-    private bool _a3;
-    public bool A3 { set => _a3 = value; }
-
-    private bool _b3;
-    public bool B3 { set => _b3 = value; }
-
-    public bool Y3 => !(_a3 && _b3);
-
-    private bool _a4;
-    public bool A4 { set => _a4 = value; }
-
-    private bool _b4;
-    public bool B4 { set => _b4 = value; }
-
-    public bool Y4 => !(_a4 && _b4);
+    public bool A4 { private get; set; }
+    public bool B4 { private get; set; }
+    public bool Y4 => !(A4 && B4);
 }
